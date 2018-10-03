5e5dd86e828b74429079d8110c0dee6890c0d7b1
jinwoongBang <dlopo123@gmail.com>
2018-10-03 11:25:12 +0900

    2018-10-3 - 방진웅 - 포인트 관련 mapper 업데이트

--- seoRak/src/main/java/kr/co/seoRak/repository/mapper/PointMapper.java
+++ seoRak/src/main/java/kr/co/seoRak/repository/mapper/PointMapper.java
@@ -6,4 +6,22 @@ import kr.co.seoRak.repository.domain.Point;
 
 public interface PointMapper {
 	List<Point> selectPointById(String memberId);
+	
+	/* [0] 전체 포인트 */
+	int selectPointTotalById(String memberId); 
+	
+	/* [1] 보낸 포인트  */
+	int selectSendPointById(String memberId);
+	
+	/* [2] 받은 포인트 */
+	int selectReceivePointById(String memberId);
+	
+	/* [3] 게시글 포인트 */
+	int selectBoardPointById(String memberId);
+	
+	/* [4] 출석 포인트 */
+	int selectAttendPointById(String memberId);
+	
+	void insertPoint(Point point);
+	
 }
