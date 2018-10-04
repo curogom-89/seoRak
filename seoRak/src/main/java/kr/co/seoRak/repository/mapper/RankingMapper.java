b0ee04fa4d9874c749c80b628e865a5e114e5af5
최창유 <chlckddb@gmail.com>
2018-10-04 09:08:04 +0900

    2010-10-04/최창유/랭킹맵퍼

--- seoRak/src/main/java/kr/co/seoRak/repository/mapper/RankingMapper.java
+++ seoRak/src/main/java/kr/co/seoRak/repository/mapper/RankingMapper.java
@@ -3,27 +3,52 @@ package kr.co.seoRak.repository.mapper;
 import java.util.Date;
 
 import kr.co.seoRak.repository.domain.RankingBookInfo;
+import kr.co.seoRak.repository.domain.RankingPointInfo;
 import kr.co.seoRak.repository.domain.RankingRankingInfo;
 
 public interface RankingMapper {
 	void insertRanking(RankingBookInfo domain);
 	String selectIsbn(String isbn);
-	void updateDate(RankingRankingInfo best);
-	void insertTotalBestRankyes24(RankingRankingInfo best);
+	void updateDate(RankingRankingInfo best);	
 	String selectIsbnbyTitle(String alaTitle);
 	String selectTitle(String string);
-	void updateBookRankingkyobo(RankingRankingInfo rankingInfo);
-	Date checkWeekDate(Date dateWeek);
-	String seorakRankingtitlecheck(String title);
-	void updateBookRankingaladdin(RankingRankingInfo rankingInfo);
-	void updateBookRankingbandi(RankingRankingInfo best);
-	void updateBookRankingbookpark(RankingRankingInfo best);
-	void updateBookRankingypbooksRank(RankingRankingInfo best);
-	void updateBookRankingmorning24Rank(RankingRankingInfo best);
-	void insertKyoboRanking(RankingRankingInfo best);
-	void insertAladdinRanking(RankingRankingInfo best);
-	void insertBookparkRanking(RankingRankingInfo best);
-	void insertBandikRanking(RankingRankingInfo best);
-	void insertYPRanking(RankingRankingInfo best);
-	String RankingTitleCheck(String title);
+	void Yes24insertRaninkingInfo(RankingRankingInfo info);
+	RankingRankingInfo rankingInfoIsBnNullCherck(String isbn);
+	Date DateCheckwhereRankingInfo(Date date);
+	void morning24UpdateRanking(RankingRankingInfo info);
+	String titleNullcheck(String alaIsbnTest);
+	String titleByIsbn(String isbn);
+	Date WeekChecking();
+	void kyoboUpdateRanking(RankingRankingInfo info);
+	void aladdinupdateRanking(RankingRankingInfo info);
+	void bandiRanking(RankingRankingInfo info);	
+	void kyoboInsert(RankingRankingInfo info);//1
+	void aladdinInsert(RankingRankingInfo info);//2
+	void bookparkInsert(RankingRankingInfo info);//3
+	void bandiInsert(RankingRankingInfo info);
+	String bookidSelectByIsbn(String result2);
+	
+	//----------------------
+	void pointInsert(RankingPointInfo point);
+	int aladdinPoint(String isbn);
+	int kyoboPoint(String isbn);
+	int bandiPoint(String isbn);
+	int yes24Point(String isbn);
+	
+	String IsbnaladdinRank(int i );
+	String IsbnkyoboRank(int i);
+	String IsbnbandiRank(int i);
+	String Isbnyes24Rank(int i);
+	
+	RankingPointInfo nullcheck(String isbn);
+	
+	int RankingselectByIsbn(int i);
+	String IsbnselectByranking(int i);
+	String authorByisbn(String isbn);
+	String selectByRankingAuthor(int i);
+	String Author(String i);
+	String selectTitle1(String i);
+	String selecPublisher(String i);
+	String selecbookimg(String i);
+	String selecpubdate(String i);
 }
